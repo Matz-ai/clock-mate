3ab768c4a4037e2442864515a178af91d5428352
TheoAntB <theo.ant.b@gmail.com>
2025-08-27 11:28:59 +0200

    Preprocessing (#9)
    * preprocessing not too dummy
    
    * preproc

--- preprocessing/README.md
+++ preprocessing/README.md
@@ -2,4 +2,11 @@ le .map de "color" remplace:
       - "w" par 0 ,
       - "b" par 1 ,
 
-le .map de "Result"
+le .map de "Result" remplace:
+      - "1-0" victoire des blancs par 0 ,
+      - "0-1" victoire des noirs par 1 ,
+      - "1/2-1/2" nul par 2 .
+
+le .map de "Termination" remplace:
+      - "Normal" remplacer par 0 ,
+      - "Time forfeit" remplacer par 1 ,
--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -11,8 +11,11 @@ def preprocess_data(df_game_info, df_moves):
     Returns:
         pd.DataFrame: The preprocessed and merged DataFrame.
     """
-    # Step 1: Drop 'Bullet' games from df_game_info
+    # Step 1: Drop unwanted game cadence from df_game_info
     df_game_info = df_game_info[(df_game_info["Cadence"] != "Bullet")]
+    df_game_info = df_game_info[df_game_info['BaseTime_s'] != 0.0]
+    df_game_info = df_game_info[~df_game_info['BaseTime_s'].isna()]
+    df_moves = df_moves[~df_moves['BaseTime_s'].isna()]
 
     # Step 2: Drop specified columns from both dataframes
     deleted_games_features = [
@@ -23,18 +26,14 @@ def preprocess_data(df_game_info, df_moves):
     df_game_info.drop(deleted_games_features, axis=1, inplace=True)
 
     deleted_moves_features = [
-        "ply", 'move_number', 'move_san', 'move_uci', 'comment', 'nags', "emt_s"
+        'move_number', 'move_san', 'move_uci', 'comment', 'nags', "emt_s"
     ]
     df_moves.drop(deleted_moves_features, axis=1, inplace=True)
 
     # Step 3: Merge the two dataframes
     df_fusion = pd.merge(df_game_info, df_moves, on="game_id")
 
-    # Step 4: Change string columns to integer
-    df_fusion["WhiteElo"] = df_fusion["WhiteElo"].astype(int)
-    df_fusion["BlackElo"] = df_fusion["BlackElo"].astype(int)
-
-    # Step 5: Encode categorical columns
+    # Step 4: Encode categorical columns
     mapping_color = {"w": 0, "b": 1}
     df_fusion["color"] = df_fusion["color"].map(mapping_color)
 
@@ -44,11 +43,22 @@ def preprocess_data(df_game_info, df_moves):
     mapping_termination = {"Normal": 0, "Time forfeit": 1}
     df_fusion["Termination"] = df_fusion["Termination"].map(mapping_termination)
 
-    # Step 6: Drop additional features for the dummy model
-    deleted_fusion_features = ['game_id', 'BaseTime_s', 'Increment_s']
+    df_fusion["eval"] = df_fusion["eval"].fillna("40000")
+
+    # Step 5: Change string columns to integer
+    df_fusion["WhiteElo"] = df_fusion["WhiteElo"].astype(int)
+    df_fusion["BlackElo"] = df_fusion["BlackElo"].astype(int)
+    df_fusion["eval"]= df_fusion["eval"].astype(int)
+
+    # Step 6: Drop additional features for the dummy mode
+    deleted_fusion_features = ['game_id', 'Increment_s']
     data_dummy = df_fusion.drop(deleted_fusion_features, axis=1)
 
     # Step 7: Fill NaN values with 0
-    data_dummy.fillna(0, inplace=True)
+    data_dummy["time_spent_s"].fillna(0, inplace=True)
 
-    return data_dummy
+    # Step 8: Keep games with minimum 2000 elo
+    filtre_elo = (df_fusion['WhiteElo'] >= 2000) | (df_fusion['BlackElo'] >= 2000)
+    data = data_dummy[filtre_elo]
+
+    return data
