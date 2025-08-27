84f152ec3eb243dca09388b4c94bd9c01fca81c4
theo berthon <theo.ant.b@gmail.com>
2025-08-27 12:56:31 +0200

    preprocessing

--- preprocessing/preprocess.py
+++ preprocessing/preprocess.py
@@ -23,7 +23,7 @@ def preprocess_data(df_game_info, df_moves):
         (df_game_info_copy['BaseTime_s'] != 0.0) &
         (~df_game_info_copy['BaseTime_s'].isna())
     ].copy()
-    
+
     df_moves_copy = df_moves_copy[~df_moves_copy['clock_s'].isna()].copy()
 
     # Step 2: Drop specified columns
@@ -62,13 +62,13 @@ def preprocess_data(df_game_info, df_moves):
 
     # Step 6: Drop additional features for the dummy mode
     deleted_fusion_features = ['game_id', 'Increment_s']
-    data_dummy = df_fusion.drop(deleted_fusion_features, axis=1)
+    data = df_fusion.drop(deleted_fusion_features, axis=1)
 
     # Step 7: Fill NaN values with 0
-    data_dummy["time_spent_s"].fillna(0, inplace=True)
+    data["time_spent_s"].fillna(0, inplace=True)
 
     # Step 8: Keep games with minimum 2000 elo
     filtre_elo = (df_fusion['WhiteElo'] >= 2000) | (df_fusion['BlackElo'] >= 2000)
-    data = data_dummy[filtre_elo]
+    data = data[filtre_elo]
 
     return data
